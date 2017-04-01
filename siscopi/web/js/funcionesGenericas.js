e6538e154b018b39a2f0c7d1a6975bcbbe551257
maricela2817 <mary171287@gmail.com>
2017-03-31 18:06:01 -0600

    trabajando en cargar asignaturas

--- siscopi/web/js/funcionesGenericas.js
+++ siscopi/web/js/funcionesGenericas.js
@@ -51,4 +51,14 @@ function siguiente2(opcion, titulo){
 	}
 }
 
+function cargaHoras(){
+	var horas= new Array("00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00");
+	var hora="";
+	for(i=0;i< horas.length;i++) {
+		hora = horas[i];
+
+		$('#thorario tbody').append('<tr><td class="text-center">'+hora+'</td><td></td><td></td><td></td><td></td><td></td><td></td></tr>')
+	}
+}
+
 
